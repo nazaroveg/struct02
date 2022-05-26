1381839c8966c2e9ef7c0c5587e4b6d68a0b48c0
NazarovEG <nazaroveg@yandex.ru>
2022-05-26 22:57:32 +0300

    struct2, v1.0

---
+++ struct02.cpp
@@ -0,0 +1,52 @@
+﻿// struct01.cpp :
+// nazaroveg@yandex.ru
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+struct address
+{
+    std::string city;
+    std::string street;
+    unsigned int  House;
+    unsigned int apartment;
+    unsigned int index;
+
+};
+
+void print(address dd);
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+
+    address add_1;
+    add_1.city = "Москва";
+    add_1.street = "проспект Мира";
+    add_1.House = 188;
+    add_1.apartment = 555;
+    add_1.index = 100100;
+    print(add_1);
+
+    address add_2;
+    add_2.city = "Клин";
+    add_2.street = "Советская";
+    add_2.House = 18;
+    add_2.apartment = 5;
+    add_2.index = 300650;
+    print(add_2);
+
+
+
+}
+void print(address dd)
+{
+    std::cout 
+         << "Город: " << dd.city << "\n"
+         << "Улица: " << dd.street << "\n"
+         << "Дом: " << dd.House << "\n"
+         << "Квартира: " << dd.apartment << "\n"
+         << "индекс: " << dd.index << "\n" << std::endl;
+}
+
